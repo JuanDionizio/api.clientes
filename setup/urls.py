9e1f9af684946478b945337444622c10ba725ef6
Juan Dionizio <juan.dionzio@gmail.com>
2024-03-29 17:22:04 -0300

    ajsute_urls1

--- setup/urls.py
+++ setup/urls.py
@@ -12,6 +12,6 @@ router.register('clientes', ClientesViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('', include('clientes.urls')),
+    path('cliente/', include('clientes.urls')),
     
 ]
